b36054998b75bdfdc314762aec00b1aff69e157b
XiaoqiangMa - Thoughtworks <xiaoqiang.ma@thoughtworks.com>
2025-09-17 11:45:03 +0800

    deploy: add cd config

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+    @GetMapping("/health")
+    public String healthy(){
+        return "I'm healthy";
+    }
 }
